84666e1a4cc1c715564eddafb84b3e966959e347
JaznanOfficial <jaznanofficial@gmail.com>
2022-01-20 08:55:17 +0600

    front page done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import './Header.css';
 import profile from './profile_1-removebg-preview (3).png'
 
@@ -11,6 +12,7 @@ const Header = () => {
                 <h1 className='text-light'>Md Abdur Rahman</h1>
                 <h3 className='text-danger'>Mern-stack Web Developer</h3>
                 <h5 className='text-light'>Khulna,Bangladesh</h5>
+                <h5><Button variant="outline-light">Admin Login</Button></h5>
             </div>
             
         </div>
--- src/Components/singleCard/SingleCard.js
+++ src/Components/singleCard/SingleCard.js
@@ -23,7 +23,7 @@ const SingleCard = (props) => {
                     
                 </div>
 
-                <div className="w-100 text-center m-2">
+                <div className="w-100 text-center m-2 d-flex flex-lg-column justify-content-center align-items-center">
                     <h1 className="mx-2"><a href={siteLink} title="Live Site"><i class="fas fa-globe text-light"></i></a></h1>
                     <h1 className="mx-2"><a href={frontSideCode} title="Client Side Code" className="mx-2"><i class="fas fa-code text-light"></i></a> <a href={backSideCode} title="Server Side Code" className="mx-2"><i class="fas fa-sitemap text-light"></i></a></h1>
                 </div>
