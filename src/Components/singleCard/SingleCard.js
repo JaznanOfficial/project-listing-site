7667e6d1337204404cb4dbc6a21cbc93c6f1ff95
JaznanOfficial <jaznanofficial@gmail.com>
2022-04-01 10:52:06 +0600

    error fixed

--- src/Components/singleCard/SingleCard.js
+++ src/Components/singleCard/SingleCard.js
@@ -5,14 +5,14 @@ import "./SingleCard.css";
 import ReactImageTooltip from "react-image-tooltip";
 
 const SingleCard = (props) => {
-    const { name, img, siteLink, frontSideCode, backSideCode, usedTechnology } = props.singleCard;
+    const { name, imgLink, siteLink, frontSideCode, serverSideCode, usedTechnology } = props.singleCard;
 
     return (
         
         <Col>
         <div className="card d-flex flex-lg-row justify-content-center align-items-center text-lg-start shadow p-3 bg-dark">
                 <div className="w-100">
-                    <ReactImageTooltip image={img}>
+                    <ReactImageTooltip image={imgLink}>
                         <h3 className="text-danger">{name}</h3>
                     </ReactImageTooltip>
                 </div>
@@ -25,7 +25,7 @@ const SingleCard = (props) => {
 
                 <div className="w-100 text-center m-2 d-flex flex-lg-column justify-content-center align-items-center">
                     <h1 className="mx-2"><a href={siteLink} title="Live Site"><i class="fas fa-globe text-light"></i></a></h1>
-                    <h1 className="mx-2"><a href={frontSideCode} title="Client Side Code" className="mx-2"><i class="fas fa-code text-light"></i></a> <a href={backSideCode} title="Server Side Code" className="mx-2"><i class="fas fa-sitemap text-light"></i></a></h1>
+                    <h1 className="mx-2"><a href={frontSideCode} title="Client Side Code" className="mx-2"><i class="fas fa-code text-light"></i></a> <a href={serverSideCode} title="Server Side Code" className="mx-2"><i class="fas fa-sitemap text-light"></i></a></h1>
                 </div>
             </div>
         </Col>
